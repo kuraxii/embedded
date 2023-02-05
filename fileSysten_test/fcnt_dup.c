a8f4b8461139cf377cf4a7d74d57ad89ca066e44
zzj <zj.zhu.cn@gmail.com>
2023-02-05 23:44:01 +0800

    3

---
+++ fileSysten_test/fcnt_dup.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <errno.h>
+int main(int argc,char *argv[])
+{
+  int fd1 = open(argv[1],O_RDWR);
+  printf("fd1 = %d\n",fd1);
+  int newfd = fcntl(fd1,F_DUPFD,0);//0被占用，fcntl使用文件描述符表中可用的最小的文件描述符返回
+  printf("newfd = %d\n",newfd);
+  int newfd2 = fcntl(fd1,F_DUPFD,7);//使用未被占用>=7的文件描述符
+  printf("newfd2 = %d\n",newfd2);
+  return 0;
+}
