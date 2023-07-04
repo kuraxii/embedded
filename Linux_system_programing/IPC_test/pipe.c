d7622000e53b4b24dc0e8c6fcfc2e17e7389608e
Null-zzj <zj.zhu.cn@gmail.com>
2023-07-04 20:11:56 +0800

    系统编程

--- Linux_system_programing/IPC_test/pipe.c
+++ Linux_system_programing/IPC_test/pipe.c
@@ -1,16 +1,15 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <fcntl.h>
-#include <errno.h>
-void sys_err(char *str){
+void sys_err(char *str) {
   perror(str);
   exit(1);
 }
-int main(int argc,char *argv[])
-{
+int main(int argc, char *argv[]) {
 
   int ret;
   pid_t pid;
@@ -20,21 +19,22 @@ int main(int argc,char *argv[])
   char buf[1024];
 
   pid = fork();
-  if(pid > 0){   //父进程
+  if (pid > 0) { // 父进程
     close(pipefd[0]);
-    
-    write(pipefd[1],str,strlen(str));
-    
+
+    write(pipefd[1], str, strlen(str));
+
     close(pipefd[1]);
-  }else if (pid == 0){ //子进程
+  } else if (pid == 0) { // 子进程
     sleep(1);
     close(pipefd[1]);
-    ret = read(pipefd[0],buf,sizeof(buf));   //写要控制写入大小，否则多余内容输出乱码
-    printf("%d\n",ret);
-    write(STDOUT_FILENO,buf,ret);
+    ret = read(pipefd[0], buf,
+               sizeof(buf)); // 写要控制写入大小，否则多余内容输出乱码
+    printf("%d\n", ret);
+    write(STDOUT_FILENO, buf, ret);
     close(pipefd[0]);
-   
-  }else{
+
+  } else {
     sys_err("fork err");
   }
   return 0;
